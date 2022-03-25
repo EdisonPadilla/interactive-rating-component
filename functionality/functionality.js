660ea6de65334ea94d6565007f98cfece080d06e
Evanny <hbpgvanny@gmail.com>
2022-03-25 16:27:45 -0500

    complete solution

--- functionality/functionality.js
+++ functionality/functionality.js
@@ -5,9 +5,6 @@ const submit_button = d.querySelector(".submit-btn");
 const thank_you_card = d.querySelector(".thank-you-state-card");
 const valores_rating = d.querySelectorAll(".rating-value-box button");
 const result = d.querySelector(".rating");
-console.log(valores_rating);
-
-console.dir(result);
 
 Array.from(valores_rating).forEach((btn) => {
   btn.addEventListener("click", (e) => {
